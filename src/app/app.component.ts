8df3072ecd4c2ff5e87478f8469fd32cf0f95249
fishstick22 <jeff.burkholder@gmail.com>
2018-01-28 20:21:07 -0600

    copied idea from stackoverflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
+interface FileReaderEventTarget extends EventTarget {
+  result: string;
+}
+
+interface FileReaderEvent extends Event {
+  target: FileReaderEventTarget;
+  getMessage(): string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular File Upload POC';
+  // https://stackoverflow.com/questions/45138466/angular-4-upload-csv
+
+  private static extractData = function(csvData) { // Input csv data to the function
+
+    const allTextLines = csvData.split(/\r\n|\n/);
+    const headers = allTextLines[0].split(',');
+    const lines = [];
+
+    for ( let i = 0; i < allTextLines.length; i++) {
+        // split content based on comma
+        const dataRow = allTextLines[i].split(',');
+        if (dataRow.length === headers.length) {
+            const tmpArr = [];
+            for ( let j = 0; j < headers.length; j++) {
+              tmpArr.push(dataRow[j]);
+            }
+            lines.push(tmpArr);
+        }
+    }
+    console.log(lines); // The data in the form of 2 dimensional array.
+  };
+
+  private handleFileSelect(fileSelectEvent) {
+
+    const fileList = fileSelectEvent.target.files;
+    const file = fileList[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(fre: FileReaderEvent) {
+      const csv = fre.target.result;
+      console.log(csv);
+      AppComponent.extractData(csv);
+    };
+
+  }
+
 }
